c586f9ddba15431bf722959f4599d4a209b565d5
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-01-08 23:27:56 -0500

    seed

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+const parseEvent = require('./lib/parse-event');
+const response = require('./lib/response');
+
+module.exports = function(handlerDir) {
+  return (event, context, callback) => {
+    let opts = parseEvent(event);
+
+    const handler = `${handlerDir}/${toHandler(opts.name)}`;
+
+    try {
+      require(handler)(opts, function(err, say) {
+        if (err) return callback(err);
+        return callback(null, response({say: say}));
+      });
+    }
+    catch (err) {
+      if (err.code === 'MODULE_NOT_FOUND') console.log(`missing handler for ${opts.name}`);
+      else {
+        console.log(err);
+        console.log(JSON.stringify(opts, null, 2));
+      }
+      callback(err);
+    }
+  };
+};
+
+function toHandler(str) {
+  return str.replace(/\./g, '').replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();}).replace(/^-/, '');
+}
---
+++ test/handlers/launch-request.js
@@ -0,0 +1,3 @@
+module.exports = function(opts, cb) {
+  return cb(null, 'why, hello there');
+}
